14c9e791c1903a59f3c55e2058f8b8aea0e81603
Aurafierro <auramariafierrofierro@gamil.com>
2024-02-22 11:50:45 -0500

    mayaya

---
+++ front-end/taller/jss/Medico.js
@@ -0,0 +1,162 @@
+var url = "http://localhost:8080/api/v1/medico/";
+
+function listarMedico() {
+  //METODO PARA LISTAR LOS CLIENTES
+  //SE CREA LA PETICION AJAX
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: function (result) {
+      //success: funcion que se ejecuta
+      //cuando la peticion tiene exito
+      console.log(result);
+
+      var cuerpoTabla = document.getElementById("cuerpoTabla");
+      //Se limpia el cuepro de la tabla
+      cuerpoTabla.innerHTML = "";
+      //se hace un ciclo que recorra l arreglo con los datos
+      for (var i = 0; i < result.length; i++) {
+        //UNA ETIQUETA tr por cada registro
+        var trResgistro = document.createElement("tr");
+
+        var celdaId = document.createElement("td");
+        let celdaTipoDocumento = document.createElement("td")
+        let celdaNumeroDocumento = document.createElement("td")
+        let celdaPrimerNombre = document.createElement("td")
+        let celdaSegundoNombre = document.createElement("td")
+        let celdaPrimerApellido = document.createElement("td")
+        let celdaSegundoApellido = document.createElement("td")
+        let celdaCorreo = document.createElement("td")
+        let celdaDireccion = document.createElement("td")
+        let celdaTelefono = document.createElement("td")
+        let celdaEstado = document.createElement("td")
+
+        let celdaOpcion = document.createElement("td");
+        let botonEditarMedico = document.createElement("button")
+        botonEditarMedico.innerHTML = "Editar"
+        botonEditarMedico.className = "btn btn-warning"
+
+        let botonDesahabilitarMedico = document.createElement("button")
+        botonDesahabilitarMedico.innerHTML = "Desahabilitar"
+        botonDesahabilitarMedico.className = "btn btn-danger"
+
+        celdaId.innerText = result[i]["id_medico"];
+        celdaTipoDocumento.innerText = result[i]["tipo_documento"];
+        celdaNumeroDocumento.innerText = result[i]["numero_documento"];
+        celdaPrimerNombre.innerText = result[i]["primer_nombre"];
+        celdaSegundoNombre.innerText = result[i]["segundo_nombre"];
+        celdaPrimerApellido.innerText = result[i]["primer_apellido"];
+        celdaSegundoApellido.innerText = result[i]["segundo_apellido"];
+        celdaCorreo.innerText = result[i]["correo"];
+        celdaDireccion.innerText = result[i]["direccion"];
+        celdaTelefono.innerText = result[i]["telefono"];
+        celdaEstado.innerText = result[i]["estado"];
+
+
+        trResgistro.appendChild(celdaId);
+        trResgistro.appendChild(celdaTipoDocumento);
+        trResgistro.appendChild(celdaNumeroDocumento);
+        trResgistro.appendChild(celdaPrimerNombre);
+        trResgistro.appendChild(celdaSegundoNombre);
+        trResgistro.appendChild(celdaPrimerApellido);
+        trResgistro.appendChild(celdaSegundoApellido);
+        trResgistro.appendChild(celdaCorreo);
+        trResgistro.appendChild(celdaDireccion);
+        trResgistro.appendChild(celdaTelefono);
+        trResgistro.appendChild(celdaEstado);
+
+
+        celdaOpcion.appendChild(botonEditarMedico);
+        trResgistro.appendChild(celdaOpcion)
+
+        celdaOpcion.appendChild(botonDesahabilitarMedico);
+        trResgistro.appendChild(celdaOpcion)
+
+        cuerpoTabla.appendChild(trResgistro);
+
+
+        //creamos un td por cada campo de resgistro
+
+      }
+    },
+    error: function (error) {
+      /*
+      ERROR: funcion que se ejecuta cuando la peticion tiene un error
+      */
+      alert("Error en la petición " + error);
+    }
+  })
+}
+
+function registrarMedico() {
+
+  let formData = {
+    "tipo_documento": document.getElementById("tipo_documento").value,
+    "numero_documento": document.getElementById("numero_documento").value,
+    "primer_nombre": document.getElementById("primer_nombre").value,
+    "segundo_nombre": document.getElementById("segundo_nombre").value,
+    "primer_apellido": document.getElementById("primer_apellido").value,
+    "segundo_apellido": document.getElementById("segundo_apellido").value,
+    "telefono": document.getElementById("telefono").value,
+    "correo": document.getElementById("correo").value,
+    "direccion": document.getElementById("direccion").value,
+    "estado": document.getElementById("estado").value
+
+  };
+
+  if (validarCampos()) {
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      success: function (result) {
+        //
+        Swal.fire({
+          title: "¡Excelente!",
+          text: "Se guardó correctamente",
+          icon: "success"
+        });
+      },
+    })
+  } else {
+    Swal.fire({
+      title: "¡Error!",
+      text: "Llene todos los campos correctamente",
+      icon: "error"
+    });
+  }
+}
+
+//se ejecuta la peticion
+
+
+function validarCampos() {
+  var numero_documento = document.getElementById("numero_documento");
+  return validarNumeroDocumento(numero_documento);
+}
+function validarNumeroDocumento(cuadroNumero) {
+  /*
+  numero documento 
+  min=5
+  max=11
+  numero entero
+
+  si cumple, se cambia color a verde
+  si no, se cambia a rojo
+  */
+  var valor = cuadroNumero.value;
+  var valido = true;
+  if (valor.length < 5 || valor.length > 11) {
+    valido = false
+  }
+
+  if (valido) {
+    //cuadro de texto cumple
+    cuadroNumero.className = "form-control is-valid";
+  } else {
+    //cuadro de texto no cumple
+    cuadroNumero.className = "form-control is-invalid";
+  }
+  return valido;
+
+}
